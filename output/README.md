8101300752bb573f2cd4d2bdcb8de4f9dd7611eb
psytrx <fj.dumont@gmail.com>
2023-10-23 15:08:25 +0200

    write an image

---
+++ output/README.md
@@ -0,0 +1,1 @@
+Output files as well as auxiliary files will be written to this directory.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use glam::uvec2;
+use image::{Rgb, RgbImage};
+
+fn main() -> anyhow::Result<()> {
+    let resolution = uvec2(800, 600);
+
+    let img = RgbImage::from_fn(resolution.x, resolution.y, |x, y| {
+        let r = x as f32 / resolution.x as f32;
+        let g = y as f32 / resolution.y as f32;
+        let b = 0.0;
+
+        let ir = (255.999 * r) as u8;
+        let ig = (255.999 * g) as u8;
+        let ib = (255.999 * b) as u8;
+
+        Rgb([ir, ig, ib])
+    });
+
+    img.save("./output/output.png")?;
+
+    Ok(())
 }
