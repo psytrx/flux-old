d73646a24ea4ed373911982b46ce661ab5e540bc
psytrx <fj.dumont@gmail.com>
2023-10-23 23:47:47 +0200

    replace naive camera with a positionable perspective camera

--- src/example_scenes/material_demo.rs
+++ src/example_scenes/material_demo.rs
@@ -5,8 +5,14 @@ use glam::{uvec2, vec3};
 use crate::flux::{Camera, MatteMaterial, MetalMaterial, Primitive, Scene, Sphere};
 
 pub fn material_demo() -> Scene {
-    let resolution = uvec2(800, 450);
-    let camera = Camera::new(resolution);
+    let camera = {
+        let resolution = uvec2(800, 450);
+        let look_from = vec3(0.0, 2.0, -8.0);
+        let look_at = vec3(0.0, 0.5, 0.0);
+        let fov = 40.0;
+
+        Camera::new(resolution, look_from, look_at, fov)
+    };
 
     let mat_floor = Rc::new(MatteMaterial::new(vec3(0.8, 0.8, 0.0)));
     let mat_left = Rc::new(MetalMaterial::new(vec3(0.8, 0.8, 0.8), 0.3));
--- src/flux/camera.rs
+++ src/flux/camera.rs
@@ -1,29 +1,48 @@
-use glam::{vec3, UVec2};
+use glam::{vec3, Mat4, UVec2, Vec3};
 
 use super::{ray::Ray, CameraSample};
 
 pub struct Camera {
     pub resolution: UVec2,
+    theta_x: f32,
+    theta_y: f32,
+    view_matrix: Mat4,
 }
 
+#[allow(dead_code)]
 impl Camera {
-    pub fn new(resolution: UVec2) -> Self {
-        Self { resolution }
+    pub fn new(resolution: UVec2, position: Vec3, look_at: Vec3, fov: f32) -> Self {
+        let view_matrix = Mat4::look_at_lh(position, look_at, Vec3::Y).inverse();
+
+        let aspect_ratio = resolution.x as f32 / resolution.y as f32;
+
+        let theta_x = (fov / 2.0).to_radians().tan();
+        let theta_y = theta_x / aspect_ratio;
+
+        Self {
+            resolution,
+            theta_x,
+            theta_y,
+            view_matrix,
+        }
     }
 
     pub fn ray(&self, sample: &CameraSample) -> Ray {
-        let look_from = vec3(0.0, 1.5, -4.0);
-        let viewport_width = 6.0;
-        let viewport_height = viewport_width / self.resolution.x as f32 * self.resolution.y as f32;
-        let top_left = vec3(-viewport_width / 2.0, viewport_height / 2.0 + 0.5, 0.0);
-        let horizontal = vec3(viewport_width, 0.0, 0.0);
-        let vertical = vec3(0.0, -viewport_height, 0.0);
-
-        let origin = look_from;
-
         let uv = sample.p_film / self.resolution.as_vec2();
-        let target = top_left + horizontal * uv.x + vertical * uv.y;
-        let direction = target - origin;
+
+        // point on the near plane, offset by uv coordinates
+        let near_plane_target = vec3(
+            -self.theta_x + 2.0 * self.theta_x * uv.x,
+            self.theta_y - 2.0 * self.theta_y * uv.y,
+            1.0,
+        );
+
+        let direction = self
+            .view_matrix
+            .transform_vector3(near_plane_target)
+            .normalize();
+
+        let origin = self.view_matrix.transform_point3(Vec3::ZERO);
 
         Ray::new(origin, direction, sample.time)
     }
