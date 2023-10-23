f0839b5215acfeaaed0dc994231cf6b007fbbc30
psytrx <fj.dumont@gmail.com>
2023-10-23 15:21:26 +0200

    extract `fn pixel_color` & `fn color_to_rgb`

--- src/flux/mod.rs
+++ src/flux/mod.rs
@@ -1,13 +1,27 @@
 pub fn render_image(resolution: glam::UVec2) -> image::RgbImage {
     image::RgbImage::from_fn(resolution.x, resolution.y, |x, y| {
-        let r = x as f32 / resolution.x as f32;
-        let g = y as f32 / resolution.y as f32;
-        let b = 0.0;
+        let uv = glam::vec2(
+            x as f32 / resolution.x as f32,
+            y as f32 / resolution.y as f32,
+        );
 
-        let ir = (255.999 * r) as u8;
-        let ig = (255.999 * g) as u8;
-        let ib = (255.999 * b) as u8;
+        let color = pixel_color(uv);
 
-        image::Rgb([ir, ig, ib])
+        color_to_rgb(color)
     })
+}
+
+fn pixel_color(uv: glam::Vec2) -> glam::Vec3 {
+    let r = uv.x;
+    let g = uv.y;
+    let b = 0.0;
+    glam::vec3(r, g, b)
+}
+
+fn color_to_rgb(color: glam::Vec3) -> image::Rgb<u8> {
+    let ir = (255.999 * color.x) as u8;
+    let ig = (255.999 * color.y) as u8;
+    let ib = (255.999 * color.z) as u8;
+
+    image::Rgb([ir, ig, ib])
 }
