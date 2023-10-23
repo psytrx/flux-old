4d0d8e0f7f917142299363b6c3aa5a554cb7d21b
psytrx <fj.dumont@gmail.com>
2023-10-23 15:22:46 +0200

    add env_logger & measure_time

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod flux;
 use glam::uvec2;
 
 fn main() -> anyhow::Result<()> {
+    env_logger::init();
+    measure_time::trace_time!("main");
+
     let resolution = uvec2(800, 600);
     let img = flux::render_image(resolution);
     img.save("./output/output.png")?;
