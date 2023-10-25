f288969e977e62ef8c05953766fb40ae8a3164a9
psytrx <fj.dumont@gmail.com>
2023-10-25 20:58:53 +0200

    simplify quad box shape

--- src/flux/shapes/quadbox.rs
+++ src/flux/shapes/quadbox.rs
@@ -1,14 +1,19 @@
 use embree4_sys::{RTCDevice, RTCGeometry};
-use glam::Vec3;
+use glam::{vec3, Vec3};
 
 use super::{Shape, TriangleMesh};
 
 pub struct QuadBox {
-    mesh: TriangleMesh,
+    shape: TriangleMesh,
 }
 
 impl QuadBox {
-    pub fn new(p: Vec3, px: Vec3, py: Vec3, pz: Vec3) -> Self {
+    pub fn new(width: f32, height: f32, depth: f32) -> Self {
+        let p = vec3(-width, -height, -depth) / 2.0;
+        let px = vec3(width, 0.0, 0.0);
+        let py = vec3(0.0, height, 0.0);
+        let pz = vec3(0.0, 0.0, depth);
+
         let vertices = vec![
             p,
             p + px,
@@ -20,20 +25,24 @@ impl QuadBox {
             p + pz + py,
         ];
         let indices = vec![
-            0, 2, 1, 0, 3, 2, 1, 2, 5, 5, 2, 6, 5, 6, 4, 6, 7, 4, 4, 7, 3, 4, 3, 0, 3, 7, 2, 2, 7,
-            6, 0, 1, 4, 4, 1, 5,
+            0, 2, 1, 0, 3, 2, // F
+            1, 2, 5, 5, 2, 6, // R
+            5, 6, 4, 6, 7, 4, // B
+            4, 7, 3, 4, 0, 3, // L
+            3, 7, 2, 2, 7, 6, // U
+            0, 1, 4, 4, 1, 5, // D
         ];
-        let mesh = TriangleMesh::new(vertices, indices);
-        Self { mesh }
+        let shape = TriangleMesh::new(vertices, indices);
+        Self { shape }
     }
 }
 
 impl Shape for QuadBox {
     unsafe fn build_geometry(&self, id: u32, device: RTCDevice) -> RTCGeometry {
-        self.mesh.build_geometry(id, device)
+        self.shape.build_geometry(id, device)
     }
 
     fn uv(&self, p: Vec3) -> glam::Vec2 {
-        self.mesh.uv(p)
+        self.shape.uv(p)
     }
 }
