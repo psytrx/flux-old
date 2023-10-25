a995ee456cb3ab8c08e044a65b2eae1a36a6ac22
psytrx <fj.dumont@gmail.com>
2023-10-25 22:44:06 +0200

    only return specular & diffuse colors in the albedo map

--- src/flux/integrators/albedo.rs
+++ src/flux/integrators/albedo.rs
@@ -1,7 +1,7 @@
 use glam::Vec3;
 use rand::rngs::StdRng;
 
-use crate::flux::{ray::Ray, Scene};
+use crate::flux::{ray::Ray, BxdfType, Scene};
 
 use super::{Integrator, LiResult};
 
@@ -18,11 +18,24 @@ impl Integrator for AlbedoIntegrator {
         match scene.intersect(ray) {
             Some(int) => {
                 let le = int.primitive.material.emitted(&int);
+
                 match int.primitive.material.scatter(ray, &int, rng) {
-                    Some(srec) => LiResult {
-                        li: le + srec.attenuation,
-                        rays: 1,
-                    },
+                    Some(srec) => {
+                        // only return the first specular or diffuse hit
+                        let t = int.primitive.material.bxdf_type();
+                        if t == BxdfType::Diffuse || t == BxdfType::Specular {
+                            LiResult {
+                                li: le + srec.attenuation,
+                                rays: 1,
+                            }
+                        } else {
+                            let result = self.li(scene, &srec.scattered, rng);
+                            LiResult {
+                                li: le + srec.attenuation * result.li,
+                                rays: 1 + result.rays,
+                            }
+                        }
+                    }
                     None => LiResult { li: le, rays: 1 },
                 }
             }
