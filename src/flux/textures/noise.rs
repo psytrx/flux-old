0103c1104db9010f777970d250b32d81469a0209
psytrx <fj.dumont@gmail.com>
2023-10-25 12:01:01 +0200

    map noise values from [-1, 1] to [0, 1]

--- src/flux/textures/noise.rs
+++ src/flux/textures/noise.rs
@@ -30,7 +30,10 @@ impl Texture<Vec3> for NoiseTexture {
         let (width, height) = self.noise.size();
         let x = (int.uv.x * width as f32) as usize;
         let y = (int.uv.y * height as f32) as usize;
+
         let v = self.noise.get_value(x, y) as f32;
+        // Map [-1, 1] to [0, 1]
+        let v = (v + 1.0) / 2.0;
         Vec3::splat(v)
     }
 }
