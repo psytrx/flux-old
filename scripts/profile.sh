752e7d7e007283042c6b1912c461b27d4fe047f0
psytrx <fj.dumont@gmail.com>
2023-10-26 20:45:00 +0200

    add valgrind profiling commands to profile script

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -2,3 +2,9 @@
 
 cargo build --profile profiling &&
 	RUST_LOG=debug samply record target/profiling/flux
+
+# cargo build --profile profiling &&
+# 	RUST_LOG=debug valgrind --tool=cachegrind ./target/release/flux --dev
+
+# cargo build --profile profiling &&
+# 	RUST_LOG=debug valgrind --tool=dhat ./target/release/flux --dev
