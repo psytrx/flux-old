76012663ffe338142d72f6ff6367679c58f4a453
psytrx <fj.dumont@gmail.com>
2023-10-27 13:19:49 +0200

    add more CLI arguments

--- README.md
+++ README.md
@@ -7,18 +7,35 @@ Physically based ray tracing adventures in Rust.
 ## Usage
 
 ```bash
-cargo build --profile release
-./target/release/flux --help
-# or:
-# cargo run --release -- --help
+$ flux --help
 
-  Usage: flux [OPTIONS] --scene <SCENE>
+Usage: flux [OPTIONS]
 
-  Options:
-    -d, --dev            Runs quick/noisy renders for iterating quickly
-    -s, --scene <SCENE>  The example scene to render
-    -h, --help           Print help
-    -V, --version        Print version
+Options:
+  -s, --scene <SCENE>
+          The example scene to render [default: cornellbox]
+      --sweeps <SWEEPS>
+          Number of full CPU sweeps to run [default: 16]
+      --spp <SPP>
+          Samples/pixel/pass [default: 16]
+      --min-depth <MIN_DEPTH>
+          Minimum tracing path depth [default: 8]
+      --max-depth <MAX_DEPTH>
+          Maximum tracing path depth [default: 32]
+      --rr-stop-prob <RR_STOP_PROB>
+          Russian roulette path termination probability [default: 0.1]
+      --aux-spp <AUX_SPP>
+          Samples/pixel/pass for auxiliary channels [default: 4]
+  -o, --out-dir <OUT_DIR>
+          Output directory for rendered images [default: ./output]
+  -u, --update-interval <UPDATE_INTERVAL>
+          Update interval for intermediate render results [default: 1]
+      --dev
+          Switch for running quick debug renders. Overrides most of the other settings
+  -h, --help
+          Print help
+  -V, --version
+          Print version
 ```
 
 ## Example Scenes
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@
 mod example_scenes;
 mod flux;
 
-use std::str::FromStr;
+use std::{
+    path::Path,
+    str::FromStr,
+    time::{Duration, Instant},
+};
 
 use crate::{
     example_scenes::{load_example_scene, ExampleScene},
@@ -22,57 +26,33 @@ use num_format::{Locale, ToFormattedString};
 use strum::ParseError;
 
 fn main() -> Result<()> {
-    let args = Args::parse();
-
     env_logger::init();
 
+    let args = Args::parse();
+
     let scene = load_scene(&args)?;
+    let renderer = setup_renderer(&args);
 
-    let renderer = {
-        let integrator = Box::new(PathTracingIntegrator::new(8, 32, 0.1));
-
-        let samples_per_pixel = if args.dev { 1 } else { 4 };
-        let sampler = StratifiedSampler::new(samples_per_pixel);
-
-        let sweeps = if args.dev { 1 } else { 4 };
-        let num_cpus = num_cpus::get();
-        let num_passes = sweeps * num_cpus;
-
-        Renderer::new(
-            integrator,
-            sampler,
-            num_passes,
-            Some(|evt| {
-                debug!("pass {}\t({:>6.3}%)", evt.passes, evt.progress);
-                evt.film
-                    .to_srgb_image()
-                    .save("./output/output.png")
-                    .unwrap();
-            }),
-        )
-    };
-
-    let t0_render = std::time::Instant::now();
+    let t_render_film = Instant::now();
     let result = {
         info!("rendering...");
         renderer.render_film(&scene)
     };
-    let elapsed = t0_render.elapsed();
-
+    let elapsed = t_render_film.elapsed();
     info!("render finished in {:.3?}", elapsed);
 
-    debug!(
-        "rays:     {:>16}",
-        result.rays.to_formatted_string(&Locale::en)
-    );
-    let rays_per_sec = (result.rays as f64 / elapsed.as_secs_f64()) as usize;
-    debug!(
-        "rays/sec: {:>16}",
-        rays_per_sec.to_formatted_string(&Locale::en)
-    );
+    print_stats(RenderStats {
+        total_rays: result.rays,
+        elapsed,
+    });
 
-    result.film.to_srgb_image().save("./output/output.png")?;
-    std::fs::copy("./output/output.png", "./output/output-raw.png")?;
+    let output_dir = Path::new(&args.out_dir);
+
+    let beauty_path = output_dir.join("output.png");
+    result.film.to_srgb_image().save(&beauty_path)?;
+
+    let beauty_raw_path = output_dir.join("output-raw.png");
+    std::fs::copy(&beauty_path, beauty_raw_path)?;
 
     let denoised = {
         info!("denoising...");
@@ -81,22 +61,18 @@ fn main() -> Result<()> {
         let albedo = {
             trace_time!("rendering albedo channel");
 
-            let result = render_aux_channel(&scene, Box::new(AlbedoIntegrator::new()), args.dev);
-            result
-                .film
-                .to_srgb_image()
-                .save("./output/output-albedo.png")?;
+            let result = render_aux_channel(&scene, Box::new(AlbedoIntegrator::new()), &args);
+            let albedo_path = output_dir.join("output-albedo.png");
+            result.film.to_srgb_image().save(&albedo_path)?;
             result.film
         };
 
         let normal = {
             trace_time!("rendering normal channel");
 
-            let result = render_aux_channel(&scene, Box::new(NormalIntegrator::new()), args.dev);
-            result
-                .film
-                .to_srgb_image()
-                .save("./output/output-normal.png")?;
+            let result = render_aux_channel(&scene, Box::new(NormalIntegrator::new()), &args);
+            let normal_path = output_dir.join("output-normal.png");
+            result.film.to_srgb_image().save(&normal_path)?;
 
             // OIDN expects normals to be in range [-1, 1], but the integrator generates colors in
             // range [0, 1], so we scale the normals here.
@@ -110,9 +86,51 @@ fn main() -> Result<()> {
         }
     };
 
-    denoised.to_srgb_image().save("./output/output.png")?;
+    denoised.to_srgb_image().save(&beauty_path)?;
 
     Ok(())
+}
+
+fn print_stats(stats: RenderStats) {
+    debug!(
+        "rays:     {:>16}",
+        stats.total_rays.to_formatted_string(&Locale::en)
+    );
+
+    let rays_per_sec = (stats.total_rays as f64 / stats.elapsed.as_secs_f64()) as usize;
+    debug!(
+        "rays/sec: {:>16}",
+        rays_per_sec.to_formatted_string(&Locale::en)
+    );
+}
+
+fn setup_renderer(args: &Args) -> Renderer {
+    let integrator = Box::new(PathTracingIntegrator::new(
+        args.min_depth,
+        args.max_depth,
+        args.rr_stop_prob,
+    ));
+
+    let sampler = StratifiedSampler::new(args.spp);
+
+    let num_passes = {
+        let num_cpus = num_cpus::get();
+        args.sweeps * num_cpus
+    };
+
+    Renderer::new(
+        integrator,
+        sampler,
+        num_passes,
+        Some(|evt| {
+            debug!("pass {}\t({:>6.3}%)", evt.passes, evt.progress);
+
+            // TODO: get out_dir from the program args into this closure
+            let beauty_path = Path::new("./output").join("output.png");
+
+            evt.film.to_srgb_image().save(&beauty_path).unwrap();
+        }),
+    )
 }
 
 fn load_scene(args: &Args) -> Result<Scene> {
@@ -124,15 +142,12 @@ fn load_scene(args: &Args) -> Result<Scene> {
     Ok(load_example_scene(example_scene))
 }
 
-fn render_aux_channel(
-    scene: &Scene,
-    integrator: Box<dyn Integrator>,
-    dev_mode: bool,
-) -> RenderResult {
-    let samples_per_pixel = if dev_mode { 1 } else { 16 };
-    let sampler = StratifiedSampler::new(samples_per_pixel);
+fn render_aux_channel(scene: &Scene, integrator: Box<dyn Integrator>, args: &Args) -> RenderResult {
+    let sampler = StratifiedSampler::new(args.aux_spp);
 
-    let passes = num_cpus::get();
+    // Currently, we only do a single sweep
+    let sweeps = 1;
+    let passes = sweeps * num_cpus::get();
     let renderer = Renderer::new(integrator, sampler, passes, None);
 
     renderer.render_film(scene)
@@ -144,18 +159,51 @@ enum FluxError {
     Scene(String, ParseError),
 }
 
+struct RenderStats {
+    total_rays: usize,
+    elapsed: Duration,
+}
+
 #[derive(Parser)]
 #[command(version)]
 pub struct Args {
     /// The example scene to render
-    #[arg(long, default_value = "cornellbox")]
-    pub scene: String,
+    #[arg(long = "scene", short = 's', default_value = "cornellbox")]
+    scene: String,
 
-    /// Samples per pixel per pass
+    ///Number of full CPU sweeps to run
+    #[arg(long = "sweeps", default_value = "16")]
+    sweeps: usize,
+
+    /// Samples/pixel/pass
     #[arg(long = "spp", default_value = "16")]
-    pub samples_per_pixel: usize,
+    spp: usize,
 
-    /// Runs quick/noisy renders for iterating quickly
+    /// Minimum tracing path depth
+    #[arg(long = "min-depth", default_value = "8")]
+    min_depth: u32,
+
+    /// Maximum tracing path depth
+    #[arg(long = "max-depth", default_value = "32")]
+    max_depth: u32,
+
+    /// Russian roulette path termination probability
+    #[arg(long = "rr-stop-prob", default_value = "0.1")]
+    rr_stop_prob: f32,
+
+    /// Samples/pixel/pass for auxiliary channels
+    #[arg(long = "aux-spp", default_value = "4")]
+    aux_spp: usize,
+
+    /// Output directory for rendered images
+    #[arg(long = "out-dir", short = 'o', default_value = "./output")]
+    out_dir: String,
+
+    /// Update interval for intermediate render results
+    #[arg(long = "update-interval", short = 'u', default_value = "1")]
+    update_interval: u32,
+
+    /// Switch for running quick debug renders. Overrides most of the other settings
     #[arg(long)]
-    pub dev: bool,
+    dev: bool,
 }
